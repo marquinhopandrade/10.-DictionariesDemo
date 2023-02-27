8b178c70121f73e71bd544aefcfa650068c6bba1
Marcos Andrade <mvpdeandrade@hotmail.com>
2023-02-27 18:38:34 -0300

    Declara classe Employee, e cria objetos a partir de um outro dicionário

--- DictionariesDemo/Program.cs
+++ DictionariesDemo/Program.cs
@@ -6,13 +6,36 @@ namespace DictionariesDemo
     {
         static void Main(string[] args)
         {
+            Employee[] employees = {
+                new Employee("CEO","Gwyn",95,200),
+                new Employee("Manager","Joe",25,35),
+                new Employee("HR","Lora",32,21),
+                new Employee("Secretary","Petra",28,18),
+                new Employee("Lead Developer","Artorias",55,35),
+                new Employee("Intern","Ernerst",22,8),
+            };
             Dictionary<int, string> myDictionary = new Dictionary<int, string>(){            
                 { 1, "one"},            
                 { 2, "two"},
-                { 3, "three"}, 
-            };            
+                { 3, "three"},
+            };
 
-            Console.WriteLine("Hello World!");
+            Dictionary<string, Employee> employeesDirectory = new Dictionary<string, Employee>();
+            foreach (Employee emp in employees)
+            {
+                employeesDirectory.Add(emp.Role, emp);
+            }
+            string key = "CEO";
+            if (employeesDirectory.ContainsKey(key))
+            {
+                Employee empl = employeesDirectory[key];
+                Console.WriteLine("Employee Name: {0}, Role: {1}, Salary: {2}", empl.Name, empl.Role, empl.Salary);
+            }
+            else
+            {
+                Console.WriteLine("No employee found with this key {0}", key) ;
+            }
+;
         }
     }
 }
