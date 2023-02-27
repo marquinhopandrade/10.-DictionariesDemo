5afac06bee61d37b3e77417eae298b943b41628c
Marcos Andrade <mvpdeandrade@hotmail.com>
2023-02-27 18:08:11 -0300

    Declara um dicionário

--- DictionariesDemo/Program.cs
+++ DictionariesDemo/Program.cs
@@ -1,11 +1,17 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace DictionariesDemo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Dictionary<int, string> myDictionary = new Dictionary<int, string>(){            
+                { 1, "one"},            
+                { 2, "two"},
+                { 3, "three"}, 
+            };            
+
             Console.WriteLine("Hello World!");
         }
     }
