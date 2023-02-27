1d48a2fd51896f6c30a1baf68d0a353078bc2067
Marcos Andrade <mvpdeandrade@hotmail.com>
2023-02-27 17:47:43 -0300

    Adicionar arquivos de projeto.

---
+++ DictionariesDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DictionariesDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
