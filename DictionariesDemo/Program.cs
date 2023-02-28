375ec4a4c195eb2ea14412c6b22cffdfe78aa6a4
Marcos Andrade <mvpdeandrade@hotmail.com>
2023-02-28 15:36:53 -0300

    Adiciona operações para atualizar e remover dados de um dicionário

--- DictionariesDemo/Program.cs
+++ DictionariesDemo/Program.cs
@@ -27,6 +27,28 @@ namespace DictionariesDemo
             {
                 employeesDirectory.Add(emp.Role, emp);
             }
+
+            //UPDATE
+            string KeyToUpdate = "HR";
+            if (employeesDirectory.ContainsKey(KeyToUpdate))
+            {
+                employeesDirectory[KeyToUpdate] = new Employee("HR", "Eleka", 26, 18);
+            }
+            else
+            {
+                //if no, print an error message
+                Console.WriteLine("No employee found with this key {0}", KeyToUpdate);
+            }
+            //REMOVE
+            string KeyToRemove = "Intern";
+            if (employeesDirectory.Remove(KeyToRemove))
+            {
+                Console.WriteLine("Employee with Role/Key {0} was Removed!.", KeyToRemove);
+            }
+            else
+            {
+                Console.WriteLine("No employees found with this Key {0}", KeyToRemove);
+            }
             for (int i = 0; i < employeesDirectory.Count; i++)
             {
                 //using ElementAt(i) to return the key-value pair stored at index i
